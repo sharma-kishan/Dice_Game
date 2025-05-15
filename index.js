2cce8e6842d6edd733b5527a854ca1dba6cc53c8
Kishan Sharma <00kishansharma22@gmail.com>
2025-05-15 18:02:42 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var num1 = Math.floor(Math.random() * 6 + 1);
+var imagePath1 = "/images/dice" + num1 + ".png";
+document.querySelector(".img1").setAttribute("src", imagePath1);
+
+var num2 = Math.floor(Math.random() * 6 + 1);
+var imagePath2 = "/images/dice" + num2 + ".png";
+document.querySelector(".img2").setAttribute("src", imagePath2);
+
+if (num1 > num2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+else if (num1 < num2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
