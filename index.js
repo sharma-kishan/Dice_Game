aef6b095812e681baa4ac243f3e730d63b059b62
Kishan Sharma <00kishansharma22@gmail.com>
2025-05-15 18:27:18 +0530

    Correct image path

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var num1 = Math.floor(Math.random() * 6 + 1);
-var imagePath1 = "/images/dice" + num1 + ".png";
+var imagePath1 = "images/dice" + num1 + ".png";
 document.querySelector(".img1").setAttribute("src", imagePath1);
 
 var num2 = Math.floor(Math.random() * 6 + 1);
-var imagePath2 = "/images/dice" + num2 + ".png";
+var imagePath2 = "images/dice" + num2 + ".png";
 document.querySelector(".img2").setAttribute("src", imagePath2);
 
 if (num1 > num2) {
